1b83a528502a4f6b58cd7ea6e6b5329fa9920e20
emytek <66952283+emytek@users.noreply.github.com>
2024-02-20 03:17:06 +0100

    Implemented event features and stripe webhooks

---
+++ app/api/webhook/stripe/route.ts
@@ -0,0 +1,219 @@
+"use server";
+
+import Stripe from "stripe";
+import {
+  CheckoutOrderParams,
+  CreateOrderParams,
+  GetOrdersByEventParams,
+  GetOrdersByUserParams,
+} from "@/types";
+import { redirect } from "next/navigation";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database";
+import Order from "../database/models/order.model";
+import Event from "../database/models/event.model";
+import { ObjectId } from "mongodb";
+import User from "../database/models/user.model";
+
+export const checkoutOrder = async (order: CheckoutOrderParams) => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+  const price = order.isFree ? 0 : Number(order.price) * 100;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            unit_amount: price,
+            product_data: {
+              name: order.eventTitle,
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        eventId: order.eventId,
+        buyerId: order.buyerId,
+      },
+      mode: "payment",
+      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+      cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+    });
+
+    redirect(session.url!);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createOrder = async (order: CreateOrderParams) => {
+  try {
+    await connectToDatabase();
+
+    const newOrder = await Order.create({
+      ...order,
+      event: order.eventId,
+      buyer: order.buyerId,
+    });
+
+    return JSON.parse(JSON.stringify(newOrder));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+// GET ORDERS BY EVENT
+export async function getOrdersByEvent({
+  searchString,
+  eventId,
+}: GetOrdersByEventParams) {
+  try {
+    await connectToDatabase();
+
+    if (!eventId) throw new Error("Event ID is required");
+    const eventObjectId = new ObjectId(eventId);
+
+    const orders = await Order.aggregate([
+      {
+        $lookup: {
+          from: "users",
+          localField: "buyer",
+          foreignField: "_id",
+          as: "buyer",
+        },
+      },
+      {
+        $unwind: "$buyer",
+      },
+      {
+        $lookup: {
+          from: "events",
+          localField: "event",
+          foreignField: "_id",
+          as: "event",
+        },
+      },
+      {
+        $unwind: "$event",
+      },
+      {
+        $project: {
+          _id: 1,
+          totalAmount: 1,
+          createdAt: 1,
+          eventTitle: "$event.title",
+          eventId: "$event._id",
+          buyer: {
+            $concat: ["$buyer.firstName", " ", "$buyer.lastName"],
+          },
+        },
+      },
+      {
+        $match: {
+          $and: [
+            { eventId: eventObjectId },
+            { buyer: { $regex: RegExp(searchString, "i") } },
+          ],
+        },
+      },
+    ]);
+
+    return JSON.parse(JSON.stringify(orders));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// GET ORDERS BY USER
+export async function getOrdersByUser({
+  userId,
+  limit = 3,
+  page,
+}: GetOrdersByUserParams) {
+  try {
+    await connectToDatabase();
+
+    const skipAmount = (Number(page) - 1) * limit;
+    const conditions = { buyer: userId };
+
+    const orders = await Order.distinct("event._id")
+      .find(conditions)
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(limit)
+      .populate({
+        path: "event",
+        model: Event,
+        populate: {
+          path: "organizer",
+          model: User,
+          select: "_id firstName lastName",
+        },
+      });
+
+    const ordersCount = await Order.distinct("event._id").countDocuments(
+      conditions
+    );
+
+    return {
+      data: JSON.parse(JSON.stringify(orders)),
+      totalPages: Math.ceil(ordersCount / limit),
+    };
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// // server.js
+// //
+// // Use this sample code to handle webhook events in your integration.
+// //
+// // 1) Paste this code into a new file (server.js)
+// //
+// // 2) Install dependencies
+// //   npm install stripe
+// //   npm install express
+// //
+// // 3) Run the server on http://localhost:4242
+// //   node server.js
+
+// // The library needs to be configured with your account's secret key.
+// // Ensure the key is kept out of any version control system you might be using.
+// const stripe = require('stripe')('sk_test_...');
+// const express = require('express');
+// const app = express();
+
+// // This is your Stripe CLI webhook secret for testing your endpoint locally.
+// const endpointSecret = "whsec_3b0fd83aed7c3a3b7862bf5887aa6d400f3fcd2371b517148faf1bc78a422d9d";
+
+// app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+//   const sig = request.headers['stripe-signature'];
+
+//   let event;
+
+//   try {
+//     event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+//   } catch (err) {
+//     response.status(400).send(`Webhook Error: ${err.message}`);
+//     return;
+//   }
+
+//   // Handle the event
+//   switch (event.type) {
+//     case 'checkout.session.completed':
+//       const checkoutSessionCompleted = event.data.object;
+//       // Then define and call a function to handle the event checkout.session.completed
+//       break;
+//     // ... handle other event types
+//     default:
+//       console.log(`Unhandled event type ${event.type}`);
+//   }
+
+//   // Return a 200 response to acknowledge receipt of the event
+//   response.send();
+// });
+
+// app.listen(4242, () => console.log('Running on port 4242'));
