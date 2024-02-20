d632647d40f04617639122dbb198fcb101b71f8a
emytek <66952283+emytek@users.noreply.github.com>
2024-02-20 03:38:17 +0100

    resolving build errors

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,170 +1,41 @@
-"use server";
+import stripe from "stripe";
+import { NextResponse } from "next/server";
+import { createOrder } from "@/lib/actions/order.actions";
 
-import Stripe from "stripe";
-import {
-  CheckoutOrderParams,
-  CreateOrderParams,
-  GetOrdersByEventParams,
-  GetOrdersByUserParams,
-} from "@/types";
-import { redirect } from "next/navigation";
-import { handleError } from "../utils";
-import { connectToDatabase } from "../database";
-import Order from "../database/models/order.model";
-import Event from "../database/models/event.model";
-import { ObjectId } from "mongodb";
-import User from "../database/models/user.model";
+export async function POST(request: Request) {
+  const body = await request.text();
 
-export const checkoutOrder = async (order: CheckoutOrderParams) => {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  const sig = request.headers.get("stripe-signature") as string;
+  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
-  const price = order.isFree ? 0 : Number(order.price) * 100;
+  let event;
 
   try {
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price_data: {
-            currency: "usd",
-            unit_amount: price,
-            product_data: {
-              name: order.eventTitle,
-            },
-          },
-          quantity: 1,
-        },
-      ],
-      metadata: {
-        eventId: order.eventId,
-        buyerId: order.buyerId,
-      },
-      mode: "payment",
-      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
-      cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
-    });
-
-    redirect(session.url!);
-  } catch (error) {
-    throw error;
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+  } catch (err) {
+    return NextResponse.json({ message: "Webhook error", error: err });
   }
-};
 
-export const createOrder = async (order: CreateOrderParams) => {
-  try {
-    await connectToDatabase();
+  // Get the ID and type
+  const eventType = event.type;
 
-    const newOrder = await Order.create({
-      ...order,
-      event: order.eventId,
-      buyer: order.buyerId,
-    });
+  // CREATE
+  if (eventType === "checkout.session.completed") {
+    const { id, amount_total, metadata } = event.data.object;
 
-    return JSON.parse(JSON.stringify(newOrder));
-  } catch (error) {
-    handleError(error);
-  }
-};
-
-// GET ORDERS BY EVENT
-export async function getOrdersByEvent({
-  searchString,
-  eventId,
-}: GetOrdersByEventParams) {
-  try {
-    await connectToDatabase();
-
-    if (!eventId) throw new Error("Event ID is required");
-    const eventObjectId = new ObjectId(eventId);
-
-    const orders = await Order.aggregate([
-      {
-        $lookup: {
-          from: "users",
-          localField: "buyer",
-          foreignField: "_id",
-          as: "buyer",
-        },
-      },
-      {
-        $unwind: "$buyer",
-      },
-      {
-        $lookup: {
-          from: "events",
-          localField: "event",
-          foreignField: "_id",
-          as: "event",
-        },
-      },
-      {
-        $unwind: "$event",
-      },
-      {
-        $project: {
-          _id: 1,
-          totalAmount: 1,
-          createdAt: 1,
-          eventTitle: "$event.title",
-          eventId: "$event._id",
-          buyer: {
-            $concat: ["$buyer.firstName", " ", "$buyer.lastName"],
-          },
-        },
-      },
-      {
-        $match: {
-          $and: [
-            { eventId: eventObjectId },
-            { buyer: { $regex: RegExp(searchString, "i") } },
-          ],
-        },
-      },
-    ]);
-
-    return JSON.parse(JSON.stringify(orders));
-  } catch (error) {
-    handleError(error);
-  }
-}
-
-// GET ORDERS BY USER
-export async function getOrdersByUser({
-  userId,
-  limit = 3,
-  page,
-}: GetOrdersByUserParams) {
-  try {
-    await connectToDatabase();
-
-    const skipAmount = (Number(page) - 1) * limit;
-    const conditions = { buyer: userId };
-
-    const orders = await Order.distinct("event._id")
-      .find(conditions)
-      .sort({ createdAt: "desc" })
-      .skip(skipAmount)
-      .limit(limit)
-      .populate({
-        path: "event",
-        model: Event,
-        populate: {
-          path: "organizer",
-          model: User,
-          select: "_id firstName lastName",
-        },
-      });
-
-    const ordersCount = await Order.distinct("event._id").countDocuments(
-      conditions
-    );
-
-    return {
-      data: JSON.parse(JSON.stringify(orders)),
-      totalPages: Math.ceil(ordersCount / limit),
+    const order = {
+      stripeId: id,
+      eventId: metadata?.eventId || "",
+      buyerId: metadata?.buyerId || "",
+      totalAmount: amount_total ? (amount_total / 100).toString() : "0",
+      createdAt: new Date(),
     };
-  } catch (error) {
-    handleError(error);
+
+    const newOrder = await createOrder(order);
+    return NextResponse.json({ message: "OK", order: newOrder });
   }
+
+  return new Response("", { status: 200 });
 }
 
 // // server.js
